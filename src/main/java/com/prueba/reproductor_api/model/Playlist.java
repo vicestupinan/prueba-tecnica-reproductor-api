4efa31274f295c2026ab62aba002e8d2bee4037d
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-06-18 11:13:12 -0500

    feat: agregar validacion notBlank al campo nombre en playlist

--- src/main/java/com/prueba/reproductor_api/model/Playlist.java
+++ src/main/java/com/prueba/reproductor_api/model/Playlist.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +29,7 @@ public class Playlist {
     private Long id;
 
     @JsonProperty("nombre")
+    @NotBlank(message = "El nombre es obligatorio")
     @Column(nullable = false, unique = true)
     private String name;
 
