969031fbc0b819d2d7da5fe0b756352eaa5b06ec
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-06-18 10:36:34 -0500

    feat: creacion de entidades para playlist y cancion

---
+++ src/main/java/com/prueba/reproductor_api/model/Playlist.java
@@ -0,0 +1,41 @@
+package com.prueba.reproductor_api.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("nombre")
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @JsonProperty("descripcion")
+    private String description;
+
+    @JsonProperty("canciones")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "playlist_id")
+    private List<Song> songs;
+}
---
+++ src/main/java/com/prueba/reproductor_api/model/Song.java
@@ -0,0 +1,38 @@
+package com.prueba.reproductor_api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("titulo")
+    private String title;
+
+    @JsonProperty("artista")
+    private String artist;
+
+    @JsonProperty("album")
+    private String album;
+
+    @JsonProperty("anno")
+    private String year;
+
+    @JsonProperty("genero")
+    private String genre;
+}
