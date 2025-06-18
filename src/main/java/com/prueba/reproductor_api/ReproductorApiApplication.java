62a1c630b7cea2907913a88a4e5579bbd6968366
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-06-18 10:04:27 -0500

    chore: proyecto base maven

---
+++ src/main/java/com/prueba/reproductor_api/ReproductorApiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.reproductor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReproductorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReproductorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/reproductor_api/ReproductorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.reproductor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReproductorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
